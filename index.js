eb4a0ff0b1917f2d30d2fb9573e6960cfc66c90a
SatyaIndraDev <satyaindrad@gmail.com>
2023-07-21 21:45:43 +0530

    sa

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const { UserRouter } = require("./Routes/user.route")
 
 
 const app=express()
+
+const cors=require('cors')
+app.use(cors())
+
 app.use(express.json())
 app.use("/users",UserRouter)
 
